6603771408ebf22efb1dffb459486f0ac09a37e4
Anuradha Mudalige <ymssanuradha@gmail.com>
2021-10-23 10:52:53 +0530

    Changes order

--- src/main/java/com/dbs/hackathon/tradingapp/model/Order.java
+++ src/main/java/com/dbs/hackathon/tradingapp/model/Order.java
@@ -10,4 +10,54 @@ public class Order {
     double price;
     String tickerId;
     int side;
+    int algoId;
+    int userId;
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTickerId() {
+        return tickerId;
+    }
+
+    public void setTickerId(String tickerId) {
+        this.tickerId = tickerId;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public int getAlgoId() {
+        return algoId;
+    }
+
+    public void setAlgoId(int algoId) {
+        this.algoId = algoId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
